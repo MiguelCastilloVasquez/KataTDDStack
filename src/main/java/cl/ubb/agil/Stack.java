42d6a4cdfd0e268d79e0da84861f34319cae0b74
MiguelCastilloVasquez <m.castillo.vas@gmail.com>
2016-05-04 11:29:28 -0300

    Exito primer test

--- src/main/java/cl/ubb/agil/Stack.java
+++ src/main/java/cl/ubb/agil/Stack.java
@@ -1,5 +1,15 @@
 package cl.ubb.agil;
 
 public class Stack {
-
+	private int tamanio;
+	public Stack(){
+		tamanio=0;
+	}
+	
+	public boolean estaVacio(){
+		if(tamanio==0){
+			return true;
+		}else
+			return false;
+	}
 }
--- src/main/test/cl/ubb/agil/TestStack.java
+++ src/main/test/cl/ubb/agil/TestStack.java
@@ -1,5 +1,16 @@
 package cl.ubb.agil;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class TestStack {
 
+	@Test
+	public void stackPartevacio(){
+		Stack stack = new Stack();
+		
+		boolean vacia = stack.estaVacio();
+		
+		assertThat(vacia,equalTo(true));
+	}
 }
